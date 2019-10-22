b04b74427081cd0db394e4d293e602fe80ca7c12
zen_n <zenhuangzheng@126.com>
2019-10-22 15:57:52 +0800

    act2do: test husky sucessful

--- package/build/util/wrapPage.tsx
+++ package/build/util/wrapPage.tsx
@@ -3,8 +3,6 @@ import React from 'react'
 import { RouteComponentProps } from '@reach/router'
 import { hot } from 'react-hot-loader/root'
 
-const a = 4
-
 const wrapPage = (Comp: React.FC, name: string): React.FC<RouteComponentProps> => {
   const KeepAliveComp: React.FC<RouteComponentProps> = (props): React.ReactElement => (
     <KeepAlive name={name}>
